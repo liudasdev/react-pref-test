b996a169308fcbba143589c1c2434c33bd4e2526
Liudas Cereskevicius <liudas.cereskevicius@adform.com>
2016-07-25 23:09:10 +0300

    react dev tools

--- src/app.jsx
+++ src/app.jsx
@@ -4,7 +4,11 @@ import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 
 import app from './reducers'
-let store = createStore(app)
+
+let store = createStore(
+	app, 
+	typeof window === 'object' && typeof window.devToolsExtension !== 'undefined' ? window.devToolsExtension() : f => f
+)
 
 import App from './components/app'
 
