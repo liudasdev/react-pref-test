760f5d90508cb3d451f75ce74d3b3235318e4f6b
Liudas Cereskevicius <liudas.cereskevicius@adform.com>
2016-07-26 12:39:02 +0300

    from last

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 
+import Perf from './perf'
 import Title from '../containers/title'
 import Items from '../containers/items'
 
 const App = () => (
   <div>
+  	<Perf />
     <Title />
     <Items />
   </div>
